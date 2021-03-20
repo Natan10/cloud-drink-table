b744bb125ffba158c9f3871d359b454aa16d465e
Natan <natan-moreira1@hotmail.com>
2021-03-20 17:36:27 -0300

    refactor: change user for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
   namespace :api, defaults: { format: :json } do
     scope module: :v1 do
-      resources :user, only: [:create]
+      resources :users, only: [:create]
+      resources :authentication, only: [:create]
     end
   end
+
 end
