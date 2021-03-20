755fe0beafcba2915dc59dc62c1af911cf8b34b8
Natan <natan-moreira1@hotmail.com>
2021-03-20 17:36:38 -0300

    chore: add configs

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class UserController < ApiController
+    class UsersController < ApiController
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
       rescue_from ActiveRecord::RecordInvalid, with: :validation_error
       
