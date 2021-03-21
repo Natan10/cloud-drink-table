f28943af6009daf7ece83155cf79ea5fba392a6d
Natan <natan-moreira1@hotmail.com>
2021-03-21 14:59:54 -0300

    refactor: add skip authentication

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UsersController < ApiController
+      skip_before_action :authenticate_user
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
       rescue_from ActiveRecord::RecordInvalid, with: :validation_error
       
