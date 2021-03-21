2d444563e108772f215a3601a6a29724f32d5e2d
Natan <natan-moreira1@hotmail.com>
2021-03-21 14:59:37 -0300

    feat: add authentication user

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,7 +1,24 @@
 module Api
   module V1
     class ApiController < ApplicationController
-      # Métodos globais
+      before_action :authenticate_user
+
+
+    private 
+
+      def token_request
+        token = request.headers['Authorization'].split(' ')[1]
+        token
+      end
+
+      def authenticate_user
+        user_id = AuthenticationTokenService.decode(token_request)
+        @current_user = User.find(user_id["user_id"])
+        @current_user
+      rescue ActiveRecord::RecordNotFound
+        head :unauthorized
+      end
+
     end
   end
 end
