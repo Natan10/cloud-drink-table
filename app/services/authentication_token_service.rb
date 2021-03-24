a0bad3f40715089a65c6b36b92ca1d7592c61516
Natan <natan-moreira1@hotmail.com>
2021-03-23 22:58:50 -0300

    feat: add exp time in auth token

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -2,13 +2,14 @@ class AuthenticationTokenService
   @token_conf = Rails.application.credentials.token  
   
   def self.encode(user_id)
-    payload = {user_id: user_id}
+    exp = (Time.now + 30.minutes).to_i
+    payload = {user_id: user_id, exp: exp}
     JWT.encode payload, @token_conf[:token_secret], @token_conf[:token_type]
   end
 
   def self.decode(token)
     decoded_token = JWT.decode token, @token_conf[:token_secret], true, {algorithm: @token_conf[:token_type]}
-    decoded_token[0]
+    decoded_token[0] 
   end
 
 end
