9304d8043694da1347c3b9d1806f19f6dbe1d95b
Natan <natan-moreira1@hotmail.com>
2021-03-28 00:35:36 -0300

    test: add should matchers in user

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ group :development, :test do
   gem "factory_bot_rails"
   gem "faker", git: "https://github.com/faker-ruby/faker.git", branch: "master"
   gem 'shoulda-matchers', '~> 4.0'
+  gem 'rspec-json_expectations'
 end
 
 group :development do
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_uniqueness_of(:email) }
+  it { should have_secure_password }
+
   describe "created user" do
     context "valid user" do
       it "valid params" do
