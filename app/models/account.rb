a6259d0bbfc35f2aa30a32215d458b19ad393ef2
Natan <natan-moreira1@hotmail.com>
2021-03-28 00:40:08 -0300

    refactor: add validation in account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -3,4 +3,5 @@ class Account < ApplicationRecord
 
   enum status: [:open, :closed]
 
+  validates :total_account, numericality: { greater_than_or_equal_to: 0.0 , less_than_or_equal_to: 999.999 }
 end
