f418b92842deeb617b802f2e23109238dbbe30e9
Natan <natan-moreira1@hotmail.com>
2021-03-28 19:56:34 -0300

    feat: add validates in consumer

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,3 +1,7 @@
 class Consumer < ApplicationRecord
   belongs_to :account
+
+  validates_presence_of :name
+  validates :total_consumed, numericality: { greater_than_or_equal_to: 0.0 , 
+    less_than_or_equal_to: 999.999 }
 end
--- spec/models/consumer_spec.rb
+++ spec/models/consumer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Consumer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:total_consumed) }
+
+  describe 'POST /create' do 
+
+  end
 end
