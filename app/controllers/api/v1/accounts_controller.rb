773039d8206f7c8f7ed0deedf36f6c73093dba78
Natan <natan-moreira1@hotmail.com>
2021-03-28 00:40:41 -0300

    feat: create account controller

---
+++ app/controllers/api/v1/accounts_controller.rb
@@ -0,0 +1,50 @@
+module Api
+  module V1
+    class AccountsController < ApiController
+
+      rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from ActiveRecord::RecordInvalid, with: :validation_user
+
+      before_action :set_account, only: [:destroy]
+      
+      def index
+        @accounts = Account.where(user_id: @current_user.id)
+        render json: {accounts: @accounts.to_json(only: [:id,:status,:description])}
+      end
+
+      def create
+        @account = Account.create!(account_params)
+        render json: @account.to_json(only: [:id,:status,:description,:total_account,:user_id]) , 
+        status: :created
+      end
+
+      def destroy 
+        @account.update(status: "closed")
+        head :ok
+      end
+
+      private 
+
+      def set_account 
+        @account = Account.find(params[:id])
+        @account
+      end
+
+      def account_params
+        params.require(:account).permit(:total_account,:status,:description,:user_id)
+      end
+
+      def parameter_missing(e)
+        render json: {
+          error: e.message
+        }, status: :unprocessable_entity
+      end
+
+      def validation_user(e)
+        render json: {
+          error: e.message
+        }, status: :unprocessable_entity
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     scope module: :v1 do
-      resources :users, only: [:create]
+      resources :users, only: [:create] do 
+        resources :accounts, only: [:index,:create,:destroy]
+      end
       resources :authentication, only: [:create]
     end
   end
