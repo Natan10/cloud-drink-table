ee30d75e6f4e1921633f0e25ebadebc8edd287dd
Natan <natan-moreira1@hotmail.com>
2021-04-11 11:39:19 -0300

    refactor: add has_many in models

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,9 @@
 class Account < ApplicationRecord
   belongs_to :user
+  has_many :consumers
 
   enum status: [:open, :closed]
 
   monetize :total_account_cents, numericality: {greater_than_or_equal_to: 0,
-                                                less_than_or_equal_to: 10000}
+                                                less_than_or_equal_to: 100000}
 end
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,8 +1,15 @@
 class Consumer < ApplicationRecord
   belongs_to :account
+  has_many :items
 
   monetize :total_consumed_cents, numericality: {greater_than_or_equal_to: 0,
-                                                 less_than_or_equal_to: 10000}
+                                                 less_than_or_equal_to: 100000}
 
   validates_presence_of :name
+
+  def total_consumer 
+    total_consumed = self.items.reduce(0) do |sum,num| 
+      sum + num.total_price
+    end
+  end
 end
