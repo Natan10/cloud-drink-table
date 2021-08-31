b008284519e4879aafbfb0dc7f789cd9d4b8e807
Natan <natan-moreira1@hotmail.com>
2021-08-31 10:02:19 -0300

    feat: create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Create user 
+
+require 'faker'
+
+puts "create users"
+5.times do |number|
+  User.create({ 
+    username: Faker::Name.name,
+    email: "user#{number}@email.com",
+    password: "123456",
+    password_confirmation: "123456",
+   })
+end
+
+puts "create restaurants"
+5.times do |number|
+  res = Restaurant.create(name: Faker::Restaurant.name)
+
+  [1,3,5,7].sample.times do |number|
+    Product.create({
+      name: Faker::Food.dish,
+      description: Faker::Food.description,
+      price: ["12.80","40.80","100.0","2.80"].sample,
+      restaurant_id: res.id
+    })
+  end
+end
