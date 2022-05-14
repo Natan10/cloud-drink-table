a51833351500aa29b766a64ecaf138e2e461ba67
Natan <natan-moreira1@hotmail.com>
2022-05-13 23:13:08 -0300

    config: change config to use sqlite and not docker-compose service

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_08_29_221626) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.integer "status", default: 0, null: false
     t.text "description"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "total_account_cents", precision: 14, scale: 2, default: "0.0"
@@ -28,8 +25,8 @@ ActiveRecord::Schema.define(version: 2021_08_29_221626) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -48,14 +45,14 @@ ActiveRecord::Schema.define(version: 2021_08_29_221626) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "consumers", force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "account_id", null: false
+    t.integer "account_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "total_consumed_cents", precision: 14, scale: 2, default: "0.0"
@@ -66,7 +63,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_221626) do
     t.string "name", null: false
     t.integer "quantity", default: 0, null: false
     t.decimal "price_cents", precision: 14, scale: 2, default: "0.0"
-    t.bigint "consumer_id", null: false
+    t.integer "consumer_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["consumer_id"], name: "index_items_on_consumer_id"
@@ -76,7 +73,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_221626) do
     t.string "name", null: false
     t.string "description"
     t.decimal "price_cents", precision: 14, scale: 2, default: "0.0"
-    t.bigint "restaurant_id", null: false
+    t.integer "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["restaurant_id"], name: "index_products_on_restaurant_id"
